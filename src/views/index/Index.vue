fa52d33538aba6a254a2fadeedf1d509f5151be3
zy <varzy@live.com>
2020-04-07 10:06:19 +0800

    添加 github 角标

--- src/views/index/Index.vue
+++ src/views/index/Index.vue
@@ -3,22 +3,37 @@
     <el-card class="readme">
       <markdown-article :md="readme"></markdown-article>
     </el-card>
+
+    <github-corner
+      url="https://github.com/varzy/vue-admin-scaffold"
+      :position="{ position: 'absolute', top: '-16px', right: '-16px' }"
+      :bg-color="primaryColor"
+    ></github-corner>
   </div>
 </template>
 
 <script>
 import MarkdownArticle from '../../components/MarkdownArticle';
 import ReadmeDoc from '../../../README.md';
+import GithubCorner from '../../components/GithubCorner';
+import { PRIMARY_COLOR } from '../../config/constants';
 
 export default {
   name: 'Index',
 
-  components: { MarkdownArticle },
+  components: { MarkdownArticle, GithubCorner },
 
   data() {
     return {
+      primaryColor: PRIMARY_COLOR,
       readme: ReadmeDoc
     };
   }
 };
 </script>
+
+<style scoped lang="scss">
+.index {
+  position: relative;
+}
+</style>
